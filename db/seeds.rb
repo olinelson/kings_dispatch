edd5f414fb5bb0a01283c5d1b6cbe4c25c13caa6
Oli Nelson <olinelson@Olis-MacBook-Pro-Uscreen.local>
2025-11-23 14:23:36 +1100

    add user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+User.create(email_address: "oli@example.com", password: "strong_password")
